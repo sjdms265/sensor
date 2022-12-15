36c5147a30bd81d0edd650e5caf49287bc3a1b2d
j m <sjdms265@protonmail.com>
2022-12-15 08:11:39 -0500

    new security config and new postgress > docker-compose.yml for local environment

--- sensormanager/src/main/java/com/sensor/sensormanager/enums/SensorType.java
+++ sensormanager/src/main/java/com/sensor/sensormanager/enums/SensorType.java
@@ -2,7 +2,7 @@ package com.sensor.sensormanager.enums;
 
 public enum SensorType {
 
-    TEMPERATURE("TEMPERATURE"), HUMIDITY("HUMIDITY");
+    TEMPERATURE("TEMPERATURE"), HUMIDITY("HUMIDITY"), HOME_ASSISTANT_STATUS("HOMEASSISTANT/STATUS");
 
     SensorType(String type) {
     }
--- sensormanager/src/main/java/com/sensor/sensormanager/security/WebSecurityConfig.java
+++ sensormanager/src/main/java/com/sensor/sensormanager/security/WebSecurityConfig.java
@@ -1,48 +1,61 @@
 package com.sensor.sensormanager.security;
 
-import com.sensor.sensormanager.filter.CustomAuthenticationFilter;
 import com.sensor.sensormanager.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-/*@Configuration
-@EnableWebSecurity*/
+/**
+ * https://www.bezkoder.com/websecurityconfigureradapter-deprecated-spring-boot/
+ * https://www.youtube.com/watch?v=b9O9NI-RJ3o
+ **/
+@Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager());
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/refreshToken").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/user/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
-        http.addFilter(customAuthenticationFilter);
+        //http.addFilter(customAuthenticationFilter);
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 
     @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder);
+
+        return authProvider;
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
+        return authConfiguration.getAuthenticationManager();
+    }
+
 }
