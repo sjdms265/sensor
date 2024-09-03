aef93da3e5b896709505592a4b18513097db4249
sjdms265 <github@localcloud.win>
2024-09-03 08:35:34 -0500

    add eureka

---
+++ sensoreureka/src/test/java/com/sensor/sensoreureka/SensoreurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sensor.sensoreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensoreurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
